e181cdfaec5aae255dbb5c6a370c8adcb6bf37cc
Cyril Ologho <cyrielo911@gmail.com>
2025-01-31 04:10:18 -0500

    refactor example project

--- example/App.tsx
+++ example/App.tsx
@@ -7,6 +7,8 @@
 
 import React from 'react';
 import type {PropsWithChildren} from 'react';
+import AnalogClock from 'react-native-analog-watch-face';
+
 import {
   SafeAreaView,
   ScrollView,
@@ -71,25 +73,13 @@ function App(): React.JSX.Element {
       <ScrollView
         contentInsetAdjustmentBehavior="automatic"
         style={backgroundStyle}>
-        <Header />
         <View
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+          <AnalogClock
+
+            />
         </View>
       </ScrollView>
     </SafeAreaView>
